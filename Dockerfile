4c16b3f90e58b62d622e218bd321b8825c80658b
Shayan Karimi <shayankarimi0078@gmail.com>
2020-12-21 19:36:14 +0330

    Dockerized the application and connected to MongoDB

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:14.15.3
+
+WORKDIR /app
+
+COPY ./package*.json /app/
+
+RUN npm install
+
+COPY . /app/
+
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const todosRouter = require('./routes/todos')
 
 const app = express()
+
+// DB connection
+mongoose
+    .connect(
+        'mongodb://mongo:27017/node-todo-api',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log('Mongo Connected'))
+    .catch(error => {
+        console.error('Connection Error: ', error.message)
+    })
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,8 +34,9 @@ app.get('/', (req, res) => {
 app.use('/todos', todosRouter)
 
 const PORT = process.env.PORT || 5000
+const HOST = '0.0.0.0'
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Server running on port ${PORT}`)
     console.log(`Visit http://localhost:${PORT}`)
 })
